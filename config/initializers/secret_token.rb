9b05f861000abd70bb9e490ee16045f39fc32dc7
pyemkey <py.emkey@gmail.com>
2014-04-19 17:40:57 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DinnerDash::Application.config.secret_key_base = 'd0c4c2423cb6e517714e532926448b610500d3ea00402b713f0957d95dbe3da2e1dfe7626eae392a37ff6418e20429aba298896c046d9b9c2fe0b2ac658deeec'
