e4cee460e5f73c64fb87848f05e45c304dd89fde
Dariusz Choruzy <darobial@gmail.com>
2014-04-21 00:46:57 +0200

    user validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :display_name, length: { in: 2..32 }
+  validates :display_name, length: { in: 2..32 }, allow_blank: true
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 DinnerDash::Application.routes.draw do
   root to: 'home#show'
-  get "home/show"
   devise_for :users
 end
