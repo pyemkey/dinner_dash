1acdc51fa01bfa51ae79bbfbaf20ab9778c84350
pyemkey <py.emkey@gmail.com>
2014-04-23 21:46:33 +0200

    Initialize cart and add helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  helper_method :cart
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
@@ -15,4 +15,16 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) << :display_name
     end
 
+  private
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+    def cart
+      if session[:cart_id]
+        @cart = Cart.find(session[:cart_id])
+      end
+    end
 end
